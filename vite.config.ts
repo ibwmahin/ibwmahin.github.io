46795763e99cea867be980f2da4d77c2a0860a68
ibwmahin <ibwmahin@gmail.com>
2025-05-11 11:01:48 +0600

    push

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/ibwmahin.github.io/",
+  base: "/ibwmahin/",
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
